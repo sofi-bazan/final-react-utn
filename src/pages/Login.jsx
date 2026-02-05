68599b30bb115f08975e758e9dba023c34e499a6
sofi21-a <sofigbazan21@gmail.com>
2026-02-05 14:21:19 -0300

    login y correcciones

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
-const Login = () => {
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "../services/firebase";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const nav = useNavigate();
+
+  // manejo de errores login
+  const login = async e => {
+  e.preventDefault();
+  try {
+      await signInWithEmailAndPassword(auth, email, pass);
+      nav("/dashboard");
+    } catch (err) {
+      console.log("Error login:", err.message);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={login}>
+      <h2>Login</h2>
+      <input placeholder="Correo electrónico" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="Contraseña" onChange={e => setPass(e.target.value)} />
+      <button>Ingresar</button>
+      <Link to="/register">Registrarse</Link>
+    </form>
+  );
 }
-
-export default Login
