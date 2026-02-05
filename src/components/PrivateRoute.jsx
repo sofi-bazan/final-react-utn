e9158acab870f582bb0f6116955464e63d952a34
sofi21-a <sofigbazan21@gmail.com>
2026-02-05 12:53:51 -0300

    pages base, privatRoute y authContext

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+import { useAuth } from "../context/AuthContext";
+
+export default function PrivateRoute({ children }) {
+    
+    const { user } = useAuth();
+
+    if (!user) return <Navigate to="/" />;
+    return children;
+}
